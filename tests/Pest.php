2ef6e8ff4684ae54fd4668db3f54b9ca0235c0c2
Andreas Schneider <schneider@mindtwo.de>
2025-05-20 17:52:49 +0200

    test php version locally

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,13 @@
 
 use mindtwo\LaravelAutoCreateUuid\Tests\TestCase;
 
-pest()
-    ->uses(TestCase::class)
-    ->in(__DIR__);
+if (function_exists('pest')) {
+    pest()
+        ->uses(TestCase::class)
+        ->in(__DIR__);
+}
+
+if (! function_exists('pest')) {
+    uses(TestCase::class)
+        ->in(__DIR__);
+}
